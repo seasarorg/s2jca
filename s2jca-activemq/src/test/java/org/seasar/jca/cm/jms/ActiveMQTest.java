0e83114b307b0394b25eb0eeb2dde08da63c9ccf
Koichi Kobayashi <koichik@improvement.jp>
2006-01-04 13:07:21 +0000

    git-svn-id: https://www.seasar.org/svn/s2jca/trunk@9 28c63342-3802-0410-b62d-930952b17a67

---
+++ s2jca-activemq/src/test/java/org/seasar/jca/cm/jms/ActiveMQTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jca.cm.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+
+import org.seasar.extension.unit.S2TestCase;
+
+/**
+ * <p>
+ * ActiveMQ の提供するリソースアダプタを使ったテスト．
+ * </p>
+ * 
+ * @author koichik
+ */
+public class ActiveMQTest extends S2TestCase {
+    protected TransactionManager tm;
+    protected ConnectionFactory cf;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        include("jms-activemq.dicon");
+    }
+
+    public void test() throws Exception {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    recvMessage();
+                    recvMessage();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    fail(e.toString());
+                }
+            }
+        });
+        thread.start();
+        Thread.sleep(1000);
+        sendMessage();
+        sendMessage();
+        thread.join();
+    }
+
+    protected void sendMessage() throws Exception {
+        tm.begin();
+        try {
+            Connection con = cf.createConnection();
+            try {
+                Session session = con.createSession(true, Session.SESSION_TRANSACTED);
+                try {
+                    MessageProducer producer = session.createProducer(session.createQueue("foo"));
+                    producer.send(session.createTextMessage("hoge"));
+                } finally {
+                    session.close();
+                }
+            } finally {
+                con.close();
+            }
+        } finally {
+            tm.commit();
+        }
+    }
+
+    protected void recvMessage() throws Exception {
+        tm.begin();
+        try {
+            Connection con = cf.createConnection();
+            try {
+                con.start();
+                Session session = con.createSession(true, Session.SESSION_TRANSACTED);
+                try {
+                    MessageConsumer consumer = session.createConsumer(session.createQueue("foo"));
+                    TextMessage msg = (TextMessage) consumer.receive();
+                    assertEquals("1", "hoge", msg.getText());
+                } finally {
+                    session.close();
+                }
+            } finally {
+                con.close();
+            }
+        } finally {
+            tm.commit();
+        }
+    }
+}
