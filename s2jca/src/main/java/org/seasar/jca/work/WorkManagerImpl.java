3dad51908ffe2dcd52c627032fdfa97e10f3e0eb
Koichi Kobayashi <koichik@improvement.jp>
2006-01-14 15:01:34 +0000

    git-svn-id: https://www.seasar.org/svn/s2jca/trunk@22 28c63342-3802-0410-b62d-930952b17a67

---
+++ s2jca/src/main/java/org/seasar/jca/work/WorkManagerImpl.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jca.work;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.resource.spi.work.ExecutionContext;
+import javax.resource.spi.work.Work;
+import javax.resource.spi.work.WorkException;
+import javax.resource.spi.work.WorkListener;
+import javax.resource.spi.work.WorkManager;
+
+import org.seasar.framework.container.annotation.tiger.Component;
+import org.seasar.framework.container.annotation.tiger.InstanceType;
+
+/**
+ * @author koichik
+ */
+@Component(instance = InstanceType.SINGLETON)
+public class WorkManagerImpl implements WorkManager {
+    protected static final int SCHEDULE_WORK = 0;
+    protected static final int START_WORK = 1;
+    protected static final int DO_WORK = 2;
+
+    protected final ExecutorService pool;
+
+    public WorkManagerImpl() {
+        this(1);
+    }
+
+    public WorkManagerImpl(final int maxThreads) {
+        this(Executors.newFixedThreadPool(maxThreads));
+    }
+
+    public WorkManagerImpl(final ExecutorService pool) {
+        this.pool = pool;
+    }
+
+    public void stop() {
+        pool.shutdown();
+    }
+
+    public void doWork(final Work work) throws WorkException {
+        executeWork(new WorkWrapper(this, work, DO_WORK));
+    }
+
+    public void doWork(final Work work, final long startTimeout,
+            final ExecutionContext execContext, final WorkListener workListener)
+            throws WorkException {
+        executeWork(new WorkWrapper(this, work, startTimeout, execContext, workListener, DO_WORK));
+    }
+
+    public long startWork(final Work work) throws WorkException {
+        return executeWork(new WorkWrapper(this, work, START_WORK));
+    }
+
+    public long startWork(final Work work, final long startTimeout,
+            final ExecutionContext execContext, final WorkListener workListener)
+            throws WorkException {
+        return executeWork(new WorkWrapper(this, work, startTimeout, execContext, workListener,
+                START_WORK));
+    }
+
+    public void scheduleWork(final Work work) throws WorkException {
+        executeWork(new WorkWrapper(this, work, SCHEDULE_WORK));
+    }
+
+    public void scheduleWork(final Work work, final long startTimeout,
+            final ExecutionContext execContext, final WorkListener workListener)
+            throws WorkException {
+        executeWork(new WorkWrapper(this, work, startTimeout, execContext, workListener,
+                SCHEDULE_WORK));
+    }
+
+    protected long executeWork(final WorkWrapper work) throws WorkException {
+        try {
+            return work.execute(pool);
+        } catch (final InterruptedException e) {
+            WorkException we = new WorkException(e);
+            we.setErrorCode(WorkException.INTERNAL);
+            throw we;
+        }
+    }
+}
---
+++ s2jca/src/main/java/org/seasar/jca/work/WorkWrapper.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jca.work;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+import javax.resource.spi.work.ExecutionContext;
+import javax.resource.spi.work.Work;
+import javax.resource.spi.work.WorkAdapter;
+import javax.resource.spi.work.WorkCompletedException;
+import javax.resource.spi.work.WorkEvent;
+import javax.resource.spi.work.WorkException;
+import javax.resource.spi.work.WorkListener;
+import javax.resource.spi.work.WorkManager;
+import javax.resource.spi.work.WorkRejectedException;
+
+/**
+ * @author koichik
+ */
+public class WorkWrapper implements Runnable {
+    protected WorkManagerImpl workManager;
+    protected Work work;
+    protected long startTimeout;
+    protected ExecutionContext execContext;
+    protected WorkListener workListener;
+    protected CountDownLatch latch;
+    protected long acceptedTime;
+    protected boolean started;
+    protected WorkException exception;
+
+    public WorkWrapper(final WorkManagerImpl workManager, final Work work, final int latchCount) {
+        this(workManager, work, WorkManager.INDEFINITE, null, new WorkAdapter(), latchCount);
+    }
+
+    public WorkWrapper(final WorkManagerImpl workManager, final Work work, final long startTimeout,
+            final ExecutionContext execContext, final WorkListener workListener,
+            final int latchCount) {
+        this.workManager = workManager;
+        this.work = work;
+        this.startTimeout = startTimeout;
+        this.execContext = execContext;
+        this.workListener = workListener;
+        this.latch = new CountDownLatch(latchCount);
+    }
+
+    public long execute(final Executor executor) throws InterruptedException {
+        acceptedTime = System.currentTimeMillis();
+        fireWorkAcceptedEvent();
+        executor.execute(this);
+        latch.await();
+        return System.currentTimeMillis() - acceptedTime;
+    }
+
+    public void release() {
+        work.release();
+    }
+
+    public void run() {
+        try {
+            started = start();
+            if (started) {
+                doWork();
+            }
+        } finally {
+            complete();
+        }
+    }
+
+    protected boolean start() {
+        try {
+            if (isTimedout()) {
+                exception = new WorkRejectedException();
+                exception.setErrorCode(WorkException.START_TIMED_OUT);
+                fireWorkRejectedEvent();
+                return false;
+            }
+            fireWorkStartedEvent();
+            return true;
+        } finally {
+            latch.countDown();
+        }
+    }
+
+    protected boolean isTimedout() {
+        if (startTimeout == WorkManager.INDEFINITE) {
+            return false;
+        }
+        if (System.currentTimeMillis() - acceptedTime < startTimeout) {
+            return false;
+        }
+        return true;
+    }
+
+    protected void doWork() {
+        try {
+            work.run();
+        } catch (final Throwable e) {
+            exception = new WorkCompletedException(e);
+            exception.setErrorCode(WorkException.UNDEFINED);
+        }
+    }
+
+    protected void complete() {
+        try {
+            if (started) {
+                fireWorkCompletedEvent();
+            }
+        } finally {
+            latch.countDown();
+        }
+    }
+
+    protected void fireWorkAcceptedEvent() {
+        if (workListener != null) {
+            workListener.workAccepted(new WorkEvent(workManager, WorkEvent.WORK_ACCEPTED, work,
+                    exception));
+        }
+    }
+
+    protected void fireWorkRejectedEvent() {
+        if (workListener != null) {
+            workListener.workRejected(new WorkEvent(workManager, WorkEvent.WORK_REJECTED, work,
+                    exception, System.currentTimeMillis() - acceptedTime));
+        }
+    }
+
+    protected void fireWorkStartedEvent() {
+        if (workListener != null) {
+            workListener.workStarted(new WorkEvent(workManager, WorkEvent.WORK_STARTED, work,
+                    exception, System.currentTimeMillis() - acceptedTime));
+        }
+    }
+
+    protected void fireWorkCompletedEvent() {
+        if (workListener != null) {
+            workListener.workCompleted(new WorkEvent(workManager, WorkEvent.WORK_COMPLETED, work,
+                    exception));
+        }
+    }
+}
