9744c72ca6fc14475f932dc7f5ac7ae47a0c985e
Koichi Kobayashi <koichik@improvement.jp>
2006-06-27 18:18:07 +0000

    git-svn-id: https://www.seasar.org/svn/s2jca/trunk@93 28c63342-3802-0410-b62d-930952b17a67

---
+++ s2jca-genericjmsra/src/test/java/org/seasar/jca/inbound/GenericJMSTest.java
@@ -0,0 +1,82 @@
+package org.seasar.jca.inbound;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.transaction.TransactionManager;
+
+import junit.framework.Assert;
+
+import org.seasar.extension.unit.S2TestCase;
+
+public class GenericJMSTest extends S2TestCase {
+
+    protected static volatile int receiveMessages;
+
+    protected TransactionManager tm;
+    protected ConnectionFactory cf;
+
+    public GenericJMSTest() {
+    }
+
+    public GenericJMSTest(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        include("jms-genericjms-inbound.dicon");
+        include("jms-genericjms-outbound.dicon");
+        receiveMessages = 0;
+    }
+
+    public void test() throws Exception {
+        for (int i = 0; i < 100; ++i) {
+            sendMessage(i);
+        }
+        int prevCount = 0;
+        synchronized (ActiveMQTest.class) {
+            while (receiveMessages != prevCount) {
+                prevCount = receiveMessages;
+                ActiveMQTest.class.wait(500);
+            }
+            assertEquals("0", 100, receiveMessages);
+        }
+    }
+
+    protected void sendMessage(int num) throws Exception {
+        tm.begin();
+        try {
+            Connection con = cf.createConnection();
+            try {
+                Session session = con.createSession(true, Session.SESSION_TRANSACTED);
+                try {
+                    MessageProducer producer = session.createProducer(session.createQueue("foo"));
+                    producer.send(session.createTextMessage(Integer.toString(num)));
+                }
+                finally {
+                    session.close();
+                }
+            }
+            finally {
+                con.close();
+            }
+        }
+        finally {
+            tm.commit();
+        }
+    }
+
+    public static class MessageListenerImpl extends Assert implements MessageListener {
+
+        public void onMessage(Message message) {
+            synchronized (ActiveMQTest.class) {
+                ++receiveMessages;
+            }
+        }
+    }
+}
---
+++ s2jca-genericjmsra/src/test/java/org/seasar/jca/outbound/GenericJMSTest.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package org.seasar.jca.outbound;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+
+import org.seasar.extension.unit.S2TestCase;
+
+
+/**
+ * @author koichik
+ *
+ */
+public class GenericJMSTest extends S2TestCase {
+    protected TransactionManager tm;
+    protected ConnectionFactory cf;
+
+    public GenericJMSTest() {
+        super();
+    }
+
+    public GenericJMSTest(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        include("jms-genericjms-outbound.dicon");
+    }
+
+    public void test() throws Exception {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    recvMessage();
+                    recvMessage();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    fail(e.toString());
+                }
+            }
+        });
+        thread.start();
+        Thread.yield();
+        sendMessage();
+        sendMessage();
+        thread.join();
+    }
+
+    protected void sendMessage() throws Exception {
+        tm.begin();
+        try {
+            Connection con = cf.createConnection();
+            try {
+                Session session = con.createSession(true, Session.SESSION_TRANSACTED);
+                try {
+                    MessageProducer producer = session.createProducer(session.createQueue("foo"));
+                    producer.send(session.createTextMessage("hoge"));
+                } finally {
+                    session.close();
+                }
+            } finally {
+                con.close();
+            }
+        } finally {
+            tm.commit();
+        }
+    }
+
+    protected void recvMessage() throws Exception {
+        tm.begin();
+        try {
+            Connection con = cf.createConnection();
+            try {
+                con.start();
+                Session session = con.createSession(true, Session.SESSION_TRANSACTED);
+                try {
+                    MessageConsumer consumer = session.createConsumer(session.createQueue("foo"));
+                    TextMessage msg = (TextMessage) consumer.receive();
+                    assertEquals("1", "hoge", msg.getText());
+                } finally {
+                    session.close();
+                }
+            } finally {
+                con.close();
+            }
+        } finally {
+            tm.commit();
+        }
+    }
+
+}
